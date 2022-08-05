5da8eeb0d2faf2c0dd09a5adcb3e7b86d4b05773
Yun Jang <jang7j@yahoo.com>
2022-08-05 12:11:01 -0500

    wrote about my favorite movie

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string name = "Sound of Music";
+int year = 1965;
+char ch = 's';
+bool goodMovie = true;
+double rating = 10;
+decimal budget = 8200000;
+
+Console.WriteLine($"{name} is the best movie of all time that was released in {year}.");
